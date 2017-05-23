c4ff8477e9e89cc4f85c61e69a8ffaa5cf29ce49
Abhishek Gupta <abhii.cse.nitdgp@gmail.com>
2017-05-23 10:39:26 +0530

    Added bigger text file

--- src/text_stats/core.clj
+++ src/text_stats/core.clj
@@ -11,6 +11,7 @@
       io/resource
       slurp))
 
+
 (defn- create-stop-words-set
   "Creates an in-memory set using the stop-words
    text file from resource"
@@ -21,6 +22,7 @@
       line-seq
       set))
 
+
 (defn- create-file-line-seq
   [filename]
   (-> filename
@@ -28,7 +30,9 @@
       io/reader
       line-seq))
 
+
 (def stop-words-set (create-stop-words-set "stopwords_en.txt"))
+
 
 ;; Read complete file in memory version (SIMPLE)
 (defn- calc-word-freqs
@@ -72,8 +76,64 @@
                   freq-map))))
 
 
-
 ;; TODO read file line by line and process
+
+(defn calc-word-freqs-line-by-line [filename]
+  (reduce #(merge-with + %1 %2) {} (->> (create-file-line-seq filename)
+       (map #(re-seq #"\w+" %))
+       (filter #(not (contains? stop-words-set %)))
+       (filter #(not (nil? %)))
+       (map frequencies))))
+
+
+
+;; TODO using transducer
+
+(def xform
+  (comp
+    (map #(re-seq #"\w+" %))
+    (filter #(not (contains? stop-words-set %)))
+    (filter #(not (nil? %)))
+    (map frequencies)))
+
+
+(defn xreducing
+  ([]
+   [])
+  ([result]
+   result)
+  ([result input] (merge-with + result input)))
+
+
+(defn calc-word-freqs-line-by-line-transducer [filename]
+  (transduce xform xreducing {} (create-file-line-seq filename)))
+
+
+(defn get-top-n-1
+  [filename n]
+  (let [freq-map (calc-word-freqs-line-by-line-transducer filename)]
+    (take n (sort-by val > freq-map))))
+
+
+(defn get-top-n-2
+  [filename n]
+  (let [freq-map (calc-word-freqs-line-by-line filename)]
+    (take n (sort-by val > freq-map))))
+
+
+
+;(defn process-with-transducer [filenames]
+;  (for [filename filenames]
+;    (transduce xform xreducing {} (create-file-line-seq filename))
+;    ))
+;(time (doall (process-with-transducer (repeat 1 "alice.txt"))))
+;(time (doall (process-with-transducer (repeat 1 "t8.shakespeare.txt"))))
+;(time (doall (for [filename (repeat 1 "t8.shakespeare.txt")]
+;               (calc-word-freqs-line-by-line filename))))
+;"Elapsed time: 1662.933099 msecs"
+
+;; TODO using transducer and pileline
+
 ;(defn- calc-word-freqs-1
 ;  "Takes a text file input and processes the same to
 ;   remove stop words and then calcs frequency of remaining
@@ -87,47 +147,6 @@
 ;    (merge-with + (async/<!! c)))
 ;
 ;    )
-
-
-;; TODO using transducer
-
-(def xform
-  (comp
-    (map #(re-seq #"\w+" %))
-    (filter #(not (contains? stop-words-set %)))
-    (filter #(not (nil? %)))
-    (map frequencies)))
-
-(defn xreducing
-  ([]
-   [])
-  ([result]
-   result)
-  ([result input] (merge-with + result input)))
-
-
-(defn process-with-transducer [filename]
-  (transduce xform xreducing {} (create-file-line-seq filename)))
-
-(defn get-top-n-1
-  "Gets the top n used words by sorting the word freq map
-  by freq value and then taking the top n eg
-
-  (get-top-n \"alice.txt\" 5)
-  => ([\"I\" 543] [\"Alice\" 396] [\"The\" 119] [\"Project\" 81] [\"Gutenberg\" 81])
-  "
-  [filename n]
-  (let [freq-map (process-with-transducer filename)]
-    (take n (sort-by val > freq-map))))
-
-;(defn process-with-transducer [filenames]
-;  (for [filename filenames]
-;    (transduce xform xreducing {} (create-file-line-seq filename))
-;    ))
-;(time (doall (for [filename (repeat 100 "alice.txt")]
-;               (calc-word-freqs filename))))
-
-;; TODO using transducer and pileline
 
 (defn process-parallel [filename]
   (a/<!!
